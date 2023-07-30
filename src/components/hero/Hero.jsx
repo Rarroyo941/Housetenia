2b9f0ca3ba7ca53b5d31950d4f365e8229a79958
Rarroyo941 <arroyo.rodrigo@outlook.com>
2023-07-29 22:49:45 -0300

    segundo commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,35 @@
 import './Style.css';
-import logo from '../../imagenes/housetenia-logo-black-sinfondo.jpg'
+import logo from '../../imagenes/housetenia-logo-black-sinfondo.jpg';
 import gimnasta from '../../imagenes/pexels-anush-gorak-1431282-removebg.jpg';
-import {AiOutlineDown} from 'react-icons/ai'
-import {FaSearchengin} from 'react-icons/fa'
+import {AiOutlineDown} from 'react-icons/ai';
+import {FaSearchengin} from 'react-icons/fa';
+import Button from 'react-bootstrap/Button';
+import React, { useEffect, useRef } from 'react';
 
 function Hero() {
+        const heroContainerRef = useRef(null);
+        const heroImgRef = useRef(null);
+      
+        useEffect(() => {
+          const heroDiv = heroContainerRef.current;
+          const paragraphs = heroDiv.querySelectorAll('p');
+          const button = heroDiv.querySelector('.hero-btn');
+      
+          paragraphs.forEach((paragraph, index) => {
+            paragraph.style.opacity = '1';
+            paragraph.style.transform = 'translateX(0)';
+            paragraph.style.transitionDelay = `${index * 0.5}s`;
+          });
+      
+          button.style.opacity = '1';
+          button.style.transform = 'translateX(0)';
+          button.style.transitionDelay = `${paragraphs.length * 0.5}s`;
+      
+          const img = heroImgRef.current;
+            img.style.opacity = '1';
+            img.style.transitionDelay = '2.5s';
+        }, []);
+
   return (
     <div className='navbar-hero-container'>
         <navbar className="navbar">
@@ -21,11 +46,18 @@ function Hero() {
             <div className='navbar-search-container'><FaSearchengin/></div>
         </navbar>
         <div className='hero-container'>
-            <div className='hero-izquierdo'>
-
+            <div ref={heroContainerRef}  className='hero-izquierdo'>
+                <p>ALCANZÁ TUS METAS</p>
+                <p>TRANSFORMÁ</p>
+                <p>TU CUERPO</p>
+                <p>Bienvenido a nuestro equipo de entrenamiento personalizado. Juntos, alcanzaremos tus metas de acondicionamiento físico con un programa adaptado a tus necesidades y objetivos.</p>
+                <p>¡Comencemos esta emocionante travesía hacia un nuevo tú!</p>
+                <Button className='hero-btn'>EMPEZÁ HOY</Button>
             </div>
             <div className='hero-derecho'>
-                <img src={gimnasta} alt='gimnasta'/>
+                <div ref={heroImgRef}className='hero-img-container'>
+                    <img src={gimnasta} alt='gimnasta'/>
+                </div>
             </div>
         </div>
     </div>
